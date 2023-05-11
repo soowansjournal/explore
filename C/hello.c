2ac03e5b7ad10450f2e457984538dcbe7e92ef3e
Soowan Choi <soowan@Soowans-MacBook-Air.local>
2023-05-11 17:40:42 -0400

    Initialize C

---
+++ C/hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    char str[200];
+
+    // Prompt user for their name
+    printf("Name: ");
+    fgets(str, 100, stdin);
+    printf("Hello, %s", str);
+
+    printf("Testing GitHub\n");
+
+}
+
